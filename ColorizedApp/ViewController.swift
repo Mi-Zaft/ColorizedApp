f5cc81741da93ca5e054c9aaa86c4d92467217f5
MiZaft <mi.zaft1@gmail.com>
2023-05-18 01:06:43 +0300

    App is ready

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,10 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet var colorizedView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorizedView.layer.cornerRadius = 10
+        sliderMoved()
+    }
+    
+    // MARK: - IBActions
+    @IBAction func sliderMoved() {
+        redValueLabel.text = String(round(redSlider.value * 100) / 100)
+        greenValueLabel.text = String(round(greenSlider.value * 100) / 100)
+        blueValueLabel.text = String(round(blueSlider.value * 100) / 100)
+        
+        colorizedView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: CGFloat(1)
+        )
     }
 
 
