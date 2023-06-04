75d7d466a154caaca82d1dfa1e2fdc739ba0f867
MiZaft <mi.zaft1@gmail.com>
2023-06-04 20:23:08 +0300

    Add textFields;

--- ColorizedApp/PaletteViewController.swift
+++ ColorizedApp/PaletteViewController.swift
@@ -20,6 +20,10 @@ class PaletteViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+    
     // MARK: - Public Properties
     var redColor = 1.0
     var greenColor = 1.0
@@ -32,9 +36,15 @@ class PaletteViewController: UIViewController {
         super.viewDidLoad()
         
         colorizedView.layer.cornerRadius = 10
-        print(greenColor)
+        
         setupValues()
         setupPreviewColor()
+        setupToolbar()
+        configureKeyboard()
+        
+        redTextField.delegate = self
+        greenTextField.delegate = self
+        blueTextField.delegate = self
     }
     
     // MARK: - IBActions
@@ -44,10 +54,13 @@ class PaletteViewController: UIViewController {
         switch sender {
         case redSlider:
             redValueLabel.text = string(from: redSlider)
+            redTextField.text = string(from: redSlider)
         case greenSlider:
             greenValueLabel.text = string(from: greenSlider)
+            greenTextField.text = string(from: greenSlider)
         default:
             blueValueLabel.text = string(from: blueSlider)
+            blueTextField.text = string(from: blueSlider)
         }
     }
     
@@ -72,6 +85,42 @@ private extension PaletteViewController {
         )
     }
     
+    func configureKeyboard() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        
+        view.addGestureRecognizer(tapGesture)
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    @objc
+    func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height / 2
+            }
+        }
+    }
+    
+    @objc
+    func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
     func setupValues() {
         redSlider.value = Float(redColor)
         greenSlider.value = Float(greenColor)
@@ -80,9 +129,87 @@ private extension PaletteViewController {
         redValueLabel.text = string(from: redSlider)
         greenValueLabel.text = string(from: greenSlider)
         blueValueLabel.text = string(from: blueSlider)
+        
+        redTextField.text = string(from: redSlider)
+        greenTextField.text = string(from: greenSlider)
+        blueTextField.text = string(from: blueSlider)
     }
     
     func string(from slider: UISlider) -> String {
         String(format: "%.2f", slider.value)
     }
+    
+    func setupToolbar() {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let flexibleSpace = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: nil,
+            action: nil
+        )
+        
+        let doneButton = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: self,
+            action: #selector(doneButtonTapped)
+        )
+        
+        toolbar.setItems([flexibleSpace, doneButton], animated: false)
+        
+        redTextField.inputAccessoryView = toolbar
+        greenTextField.inputAccessoryView = toolbar
+        blueTextField.inputAccessoryView = toolbar
+    }
+    
+    @objc
+    func doneButtonTapped() {
+        hideKeyboard()
+    }
+    
+    @objc
+    func hideKeyboard() {
+        view.endEditing(true)
+    }
 }
+
+// MARK: - UITextFieldDelegate
+extension PaletteViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentText = textField.text ?? ""
+        let updatedText = (currentText as NSString).replacingCharacters(
+            in: range,
+            with: string
+        )
+        
+        if let number = Double(updatedText) {
+            let minValue = 0.0
+            let maxValue = 1.0
+            
+            switch textField {
+            case redTextField:
+                redSlider.setValue(Float(number), animated: true)
+                redValueLabel.text = String(number)
+            case greenTextField:
+                greenSlider.setValue(Float(number), animated: true)
+                greenValueLabel.text = String(number)
+            default:
+                blueSlider.setValue(Float(number), animated: true)
+                blueValueLabel.text = String(number)
+            }
+            
+            setupPreviewColor()
+            
+            return (number >= minValue && number <= maxValue)
+        }
+        
+        return false
+    }
+}
+
+
+
+
+
+
+
